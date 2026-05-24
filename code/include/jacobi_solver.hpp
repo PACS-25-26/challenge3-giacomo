4f3a1e5422aa544d5fa595223e4fa91cb79dc4ea
riccardo mietto <riccardo.mietto@mail.polimi.it>
2026-05-24 16:39:20 +0200

    An object oriented implementation

--- code/include/jacobi_solver.hpp
+++ code/include/jacobi_solver.hpp
@@ -14,21 +14,43 @@
 
 namespace parallel_jacobi{
 
+    using MatrixRowMaj = Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor>;
+
     class JacobiSolver{
         
         private:
-            int num_elem;
-            double x_iniz;
-            double y_iniz;
-            double x_fin;
-            double y_fin:
+            int n;
+            double h;
+            double x_iniz, y_iniz, x_fin, y_fin;
             std::function<double(double, double)> f;
             std::function<double(double, double)> g;
 
+            MPI_Comm comm;
+            int rank, size;
+            int local_rows;
+            int up_neighbor, down_neighbor;
+            int start_i, end_i;
+            std::vector<int> sendcounts;
+            std::vector<int> displs;
+
+            MatrixRowMaj U_global; 
+            MatrixRowMaj U_loc0;
+            MatrixRowMaj U_loc1;
+
+            void initialize_global_bc();
+            void exchange_halos();
+            void compute_step();
+            double compute_error_and_update();
+
         public:
-            JacobiSolver(int n, int rank, int size);
+            JacobiSolver(int n, double x_i, double y_i, double x_f, double y_f,
+                               std::function<double(double, double)> force,
+                               std::function<double(double, double)> bc,
+                               MPI_Comm communicator);
 
             void solve(int max_iters, double tolerance);
+
+            MatrixRowMaj get_global_matrix() const;
     };
 
 }
--- code/include/utils.hpp
+++ code/include/utils.hpp
@@ -11,15 +11,39 @@
 #include <Eigen/Dense>
 #include <vector>
 #include <functional>
+#include "jacobi_solver.hpp"
 
 namespace parallel_jacobi{
 
-    double exact_solution(double x, double y);
-    double forcing_term(double x, double y);
-    double boundary_condition(double x, double y);
-    double compute_L2_error(const Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor>& U,
+    double exact_solution(double x, double y){
+        return std::sin(2.0 * M_PI * x) * std::sin(2.0 * M_PI * y);
+    }
+
+    double forcing_term(double x, double y){
+        return 8.0 * M_PI * M_PI * std::sin(2.0 * M_PI * x) * std::sin(2.0 * M_PI * y);
+    }
+
+    double boundary_condition(double x, double y){
+        return 0.0;
+    }
+
+    double compute_L2_error(const MatrixRowMaj U,
                             int n, double h, double x_iniz, double y_iniz,
-                            std::function<double(double, double)> exact_u);
+                            std::function<double(double, double)> exact_u){
+        double sum = 0.0;
+        
+        for (int i = 1; i < n - 1; i++) {
+            for (int j = 1; j < n - 1; j++) {
+                double x_val = x_iniz + j * h;
+                double y_val = y_iniz + i * h;
+                
+                double diff = U(i, j) - exact_u(x_val, y_val);
+                sum += (diff * diff);
+            }
+        }
+        
+        return std::sqrt(h * sum);
+    }
 }
 
 
